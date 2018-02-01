41189150c4180eb1723e612fafdb7b6887bfef96
Robin Křenecký <rkrenecky@gmail.com>
2018-02-01 16:48:51 +0100

    Update FSCalendarCell.m

--- FSCalendar/FSCalendarCell.m
+++ FSCalendar/FSCalendarCell.m
@@ -132,7 +132,7 @@
     _imageView.frame = CGRectMake(self.preferredImageOffset.x, self.preferredImageOffset.y, self.contentView.fs_width, self.contentView.fs_height);
     
     CGFloat titleHeight = self.bounds.size.height*5.0/6.0;
-    CGFloat diameter = MIN(self.bounds.size.height*5.0/6.0,self.bounds.size.width);
+    CGFloat diameter = self.bounds.size.height/1.85;
     diameter = diameter > FSCalendarStandardCellDiameter ? (diameter - (diameter-FSCalendarStandardCellDiameter)*0.5) : diameter;
     _shapeLayer.frame = CGRectMake((self.bounds.size.width-diameter)/2,
                                    (titleHeight-diameter)/2,
